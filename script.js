4b2b11a3b4f83c36f40dd276357fe5e8b0216c5a
Kancheti Venkata Rao <venkataraookancheti@gmail.com>
2026-04-03 15:21:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const dish = document.getElementById('dish');
+const dish1 = document.getElementById('dish1');
+const dish2 = document.getElementById('dish2');
+const dish3 = document.getElementById('dish3');
+const dish4 = document.getElementById('dish4');
+const dish5 = document.getElementById('dish5');
+const dish6 = document.getElementById('dish6');
+
+dish1.addEventListener('click', () =>{
+     dish.src="./images/dish1.jpg"
+})
+dish2.addEventListener('click', () =>{
+    dish.src="./images/dish2.jpg"
+})
+dish3.addEventListener('click', () =>{
+    dish.src="./images/dish3.jpg"
+})
+dish4.addEventListener('click', () =>{
+    dish.src="./images/dish4.jpg"
+})
+dish5.addEventListener('click', () =>{
+    dish.src="./images/dish5.jpg"
+})
+dish6.addEventListener('click', () =>{
+    dish.src="./images/dish6.jpg"
+})
